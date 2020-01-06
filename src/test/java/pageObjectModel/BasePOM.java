70ca536f799d7a9d2b35c1f9e8e9830c41096412
Ekta Nag <ektacs42@gmail.com>
2020-01-05 21:53:43 -0500

    Adding Rogers TestCase file

--- src/test/java/pageObjectModel/BasePOM.java
+++ src/test/java/pageObjectModel/BasePOM.java
@@ -3,6 +3,7 @@ package pageObjectModel;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class BasePOM
 {
@@ -10,26 +11,43 @@ public class BasePOM
     WebDriver driver;
     //Make a constructor to initialize value of driver
     public BasePOM(WebDriver d)
+
     {
         driver=d;
     }
 
-    public void getText(By locator)
+    public String getText(By locator)
     {
-        driver.findElement(locator).getText();
+        return driver.findElement(locator).getText();
     }
+
 
     public void sendKeys(By locator, String name)
     {
         driver.findElement(locator).sendKeys(name);
     }
 
+    public void clearPlaceholder(By locator)
+    {
+        driver.findElement(locator).clear();
+    }
+
+
     public void click(By locator)
     {
         driver.findElement(locator).click();
     }
 
+public String getCurrentURL()
+{
+    return driver.getCurrentUrl();
+}
 
+public void hoverMouse(WebElement element)
+{
+    Actions action = new Actions(driver);
+    action.moveToElement(element).build().perform();
+}
 
 
 }
