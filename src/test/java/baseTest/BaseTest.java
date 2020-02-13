8b8d5396e42a2a768f9fc3988e0055a153e56cf9
Ekta Nag Sharma <ektacs42@gmail.com>
2020-02-13 12:37:08 -0500

    Added the Extent Report to the baseTest class file

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,45 +1,85 @@
 package baseTest;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.SkipException;
+import org.testng.annotations.*;
 import shared.Utils;
 
 import java.lang.reflect.Method;
-import java.util.concurrent.TimeUnit;
 
-public class BaseTest {
+public class BaseTest  {
 
     String testEnv = "www.rogers.com";
     WebDriver driver;
+    ExtentReports report;
+    ExtentHtmlReporter htmlReporter;
+    ExtentTest extentTest;
+
+
+    @BeforeSuite
+    public void startUpReport()
+    {
+         report=new ExtentReports();
+         htmlReporter=new ExtentHtmlReporter("F:\\GitHub25thJan2020\\ExtentReportSeleniumAutomationProject\\ExtentReport.html");
+        htmlReporter.config().setDocumentTitle("Title of the Report Comes here");
+        htmlReporter.config().setReportName("Name of the Report Comes here");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+
+         report.attachReporter(htmlReporter);
+
+        report.setSystemInfo("Host Name", "Adding the Extent Report Version 3");
+        report.setSystemInfo("Environment", "Automation Testing");
+        report.setSystemInfo("User Name", "Ekta Nag");
+
+
+
+    }
 
     @BeforeMethod(alwaysRun = true)
     public void setUp(Method method)
     {
+        extentTest = report.createTest(method.getName());
+
         driver=Utils.browserLaunch("local");
         System.out.println("The method name is : "+method.getName());
         System.out.println();
     }
 
-
     @AfterMethod(alwaysRun = true)
     public void closeBrowser(ITestResult result)
     {
-        if (result.isSuccess())
-        {
-            System.out.println("TEST IS PASSED");
+        if(result.getStatus() == ITestResult.FAILURE){
+
+            extentTest.log(Status.FAIL, MarkupHelper.createLabel(result.getName() + " - Test Case Failed", ExtentColor.RED));
+            extentTest.log(Status.FAIL, MarkupHelper.createLabel(result.getThrowable() + " - Test Case Failed", ExtentColor.RED));
         }
-        else
-        {
-            System.out.println("TEST FAILED");
+        else if(result.getStatus() == ITestResult.SKIP){
+
+            extentTest.log(Status.SKIP, MarkupHelper.createLabel(result.getName() + " - Test Case Skipped", ExtentColor.ORANGE));
         }
-  driver.quit();
+        else {
+            extentTest.log(Status.PASS, MarkupHelper.createLabel(result.getName() + "Test is passed", ExtentColor.GREEN));
+        }
+            driver.quit();
+
     }
 
+    @AfterSuite
+    public void endReport() {
+        report.flush();
 
+    }
 }
 
 
